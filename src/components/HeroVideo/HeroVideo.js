ed4c8c41a2e91aaa21c4e761f4cb975e7b0e0c7e
Kyle Robinson <kylerobbo12@gmail.com>
2019-01-26 18:52:06 +0000

    add vimeo sdk player

--- src/components/HeroVideo/HeroVideo.js
+++ src/components/HeroVideo/HeroVideo.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import HeroVideoStyles from "./HeroVideoStyles.module.css"
+import Player from '@vimeo/player';
 
 export default class HeroVideo extends React.Component{
   state = {
@@ -9,6 +10,20 @@ export default class HeroVideo extends React.Component{
 
 
   componentDidMount(){
+
+    const iframe = document.querySelector('iframe');
+
+    const options = {
+    id: 100237575,
+    background: true,
+    loop: true
+    };
+
+    const player = new Player('hero', options);
+
+    player.on('play', function() {
+    console.log('played the video!');
+    });
 
     // let container = document.querySelectorAll('.heroVideo')[0];
     let url=`https://vimeo.com/api/oembed.json?url=https://vimeo.com/100237575&api=false&loop=true&portrait=false&title=false&autoplay=true&byline=false`
